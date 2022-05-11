e56f435d0ae1083edb094572c9382a6507a5f032
Noah Bate <92116978+noahdoespython@users.noreply.github.com>
2022-05-11 17:42:14 +0100

    Create SDK

---
+++ src/init.lua
@@ -0,0 +1,56 @@
+local SDK = {}
+local BASE_URL = "https://api.parcelroblox.com/";
+local fetch = game:GetService('HttpService')
+
+
+function SDK.CheckProductWhitelist(hubID : string, productID : string, robloxID: string)
+    local URL = BASE_URL .. "api/wl/check";
+    local data = {
+        hubID = hubID,
+        productID = productID,
+        robloxID = robloxID
+    }
+    local finaldata = fetch:JSONEncode(data)
+    local response = fetch:PostAsync(URL, finaldata)
+    print(response)
+    
+end
+
+function AddUserToWhitelist(hub_secret_key : string, productID : string, robloxID: string)
+    local URL = BASE_URL .. "api/wl/add"
+    local header = {
+        hub_secret_key = hub_secret_key,
+    }
+    local finalheader = fetch:JSONEncode(header)
+
+    local data = {
+        productID = productID,
+        robloxID = robloxID
+    }
+    local finaldata = fetch:JSONEncode(data)
+    local response = fetch:PostAsync(URL, finaldata, Enum.HttpContentType.ApplicationJson, finalheader)
+    print(response)
+end
+
+function GetProductsList(hub_secret_key : string)
+    local URL = BASE_URL .. "api/hub/getproducts"
+    local header = {
+        hub_secret_key = hub_secret_key
+    }
+    local finalheader = fetch:JSONEncode(header)
+    local response = fetch:GetAsync(URL, finalheader)
+    print(response)
+end
+
+function GetSpecificProducts(robloxID: string, hub_secret_key : string)
+    local URL = BASE_URL .. "api/hub/getproducts/"..robloxID
+    local header = {
+        hub_secret_key = hub_secret_key
+    }
+    local finalheader = fetch:JSONEncode(header)
+    local response = fetch:GetAsync(URL, finalheader)
+    print(response)
+    
+end
+
+return SDK
